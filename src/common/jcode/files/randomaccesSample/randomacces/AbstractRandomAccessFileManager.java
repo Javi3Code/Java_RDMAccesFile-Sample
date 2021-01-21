0ffa90fe53559a3aa245d2c84012066585355652
@JeyCode <j.code.initialize@gmail.com>
2021-01-21 13:06:07 +0100

    Arreglando formato

--- src/common/jcode/files/randomaccesSample/randomacces/AbstractRandomAccessFileManager.java
+++ src/common/jcode/files/randomaccesSample/randomacces/AbstractRandomAccessFileManager.java
@@ -11,102 +11,102 @@ import java.util.stream.Stream;
 public abstract class AbstractRandomAccessFileManager<T>
 {
 
-						private static final String UTF_8 = "UTF-8";
-						protected static final String SUFFIX = "s";
-						protected static final String PREFIX = "%-";
-						protected static final String STRING_EMPTY = "";
-						protected Map<String,Integer> fieldMap;
-						protected static final String RWS = "rws";
-						protected final File file;
-						protected final long recordLength;
-						protected long rowsOfRecords;
+      private static final String UTF_8 = "UTF-8";
+      protected static final String SUFFIX = "s";
+      protected static final String PREFIX = "%-";
+      protected static final String STRING_EMPTY = "";
+      protected Map<String,Integer> fieldMap;
+      protected static final String RWS = "rws";
+      protected final File file;
+      protected final long recordLength;
+      protected long rowsOfRecords;
 
-						public AbstractRandomAccessFileManager(Map<String,Integer> fieldMap,File file)
-						{
-												this.fieldMap = fieldMap;
-												this.file = file;
-												recordLength = size(fieldMap);
-												rowsOfRecords = file.length() / recordLength;
-						}
+      public AbstractRandomAccessFileManager(Map<String,Integer> fieldMap,File file)
+      {
+            this.fieldMap = fieldMap;
+            this.file = file;
+            recordLength = size(fieldMap);
+            rowsOfRecords = file.length() / recordLength;
+      }
 
-						public abstract T get(String index) throws IOException;
+      public abstract T get(String index) throws IOException;
 
-						public abstract T get(int index) throws IOException;
+      public abstract T get(int index) throws IOException;
 
-						public boolean save(Map<String,String> record)
-						{
-												var index = rowsOfRecords;
-												return save(record,index);
-						}
+      public boolean save(Map<String,String> record)
+      {
+            var index = rowsOfRecords;
+            return save(record,index);
+      }
 
-						public boolean save(Map<String,String> record,long index)
-						{
-												var successfulInsert = false;
-												try (var randomAccess = new RandomAccessFile(file,RWS))
-												{
-																		randomAccess.seek(index * recordLength);
+      public boolean save(Map<String,String> record,long index)
+      {
+            var successfulInsert = false;
+            try (var randomAccess = new RandomAccessFile(file,RWS))
+            {
+                  randomAccess.seek(index * recordLength);
 
-																		successfulInsert = fieldMap.entrySet()
-																																													.stream()
-																																													.flatMap(entry-> returnDataFormat(entry,record))
-																																													.map(mappedEntry-> savedData(mappedEntry,randomAccess))
-																																													.reduce(Boolean::logicalAnd)
-																																													.get();
-																		rowsOfRecords++;
-												}
-												catch (IOException ex)
-												{
-																		successfulInsert = false;
-												}
-												return successfulInsert;
-						}
+                  successfulInsert = fieldMap.entrySet()
+                                             .stream()
+                                             .flatMap(entry-> returnDataFormat(entry,record))
+                                             .map(mappedEntry-> savedData(mappedEntry,randomAccess))
+                                             .reduce(Boolean::logicalAnd)
+                                             .get();
+                  rowsOfRecords++;
+            }
+            catch (IOException ex)
+            {
+                  successfulInsert = false;
+            }
+            return successfulInsert;
+      }
 
-						private Stream<Entry<Integer,String>> returnDataFormat(Entry<String,Integer> entry,Map<String,String> record)
-						{
-												var recordFieldValue = record.get(entry.getKey());
-												var fieldValue = Objects.isNull(recordFieldValue) ? STRING_EMPTY : recordFieldValue;
-												var fieldLength = entry.getValue();
-												var formattedValue = String.format(PREFIX + fieldLength + SUFFIX,fieldValue);
-												
-												return Map.of(fieldLength,formattedValue)
-																						.entrySet()
-																						.stream();
-						}
+      private Stream<Entry<Integer,String>> returnDataFormat(Entry<String,Integer> entry,Map<String,String> record)
+      {
+            var recordFieldValue = record.get(entry.getKey());
+            var fieldValue = Objects.isNull(recordFieldValue) ? STRING_EMPTY : recordFieldValue;
+            var fieldLength = entry.getValue();
+            var formattedValue = String.format(PREFIX + fieldLength + SUFFIX,fieldValue);
 
-						private boolean savedData(Entry<Integer,String> mappedEntry,RandomAccessFile randomAccess)
-						{
-												try
-												{
-																		var bytesToWrite = mappedEntry.getValue()
-																																																.getBytes(UTF_8);
-																		var length = mappedEntry.getKey();
+            return Map.of(fieldLength,formattedValue)
+                      .entrySet()
+                      .stream();
+      }
 
-																		randomAccess.write(bytesToWrite,0,length);
-																		return true;
-												}
-												catch (IOException ex)
-												{
-																		return false;
-												}
-						}
+      private boolean savedData(Entry<Integer,String> mappedEntry,RandomAccessFile randomAccess)
+      {
+            try
+            {
+                  var bytesToWrite = mappedEntry.getValue()
+                                                .getBytes(UTF_8);
+                  var length = mappedEntry.getKey();
 
-						private Long size(Map<String,Integer> fieldMap)
-						{
-												return Objects.requireNonNull(fieldMap)
-																										.entrySet()
-																										.stream()
-																										.map(entry-> (long)entry.getValue())
-																										.reduce((long)0,Long::sum);
-						}
+                  randomAccess.write(bytesToWrite,0,length);
+                  return true;
+            }
+            catch (IOException ex)
+            {
+                  return false;
+            }
+      }
 
-						public Map<String,Integer> getFieldMap()
-						{
-												return fieldMap;
-						}
+      private Long size(Map<String,Integer> fieldMap)
+      {
+            return Objects.requireNonNull(fieldMap)
+                          .entrySet()
+                          .stream()
+                          .map(entry-> (long)entry.getValue())
+                          .reduce((long)0,Long::sum);
+      }
 
-						public void setFieldMap(Map<String,Integer> fieldMap)
-						{
-												this.fieldMap = Objects.requireNonNull(fieldMap);
-						}
+      public Map<String,Integer> getFieldMap()
+      {
+            return fieldMap;
+      }
+
+      public void setFieldMap(Map<String,Integer> fieldMap)
+      {
+            this.fieldMap = Objects.requireNonNull(fieldMap);
+      }
 
 }
--- src/common/jcode/files/randomaccesSample/randomacces/RandomAccessCakesManager.java
+++ src/common/jcode/files/randomaccesSample/randomacces/RandomAccessCakesManager.java
@@ -17,149 +17,149 @@ import common.jcode.files.randomaccesSample.pojos.Cake;
 public class RandomAccessCakesManager extends AbstractRandomAccessFileManager<Cake>
 {
 
-						private static final String SEPARATOR = ",";
-						public static final String PRICE = "PRICE";
-						public static final String INGREDIENTS = "INGREDIENTS";
-						public static final String DESCRIPTION = "DESCRIPTION";
-						public static final String NAME = "NAME";
-						private final File indexFile;
-						private final Properties indexManager;
-						public static Map<String,Integer> fieldMap = cakeMap();
+      private static final String SEPARATOR = ",";
+      public static final String PRICE = "PRICE";
+      public static final String INGREDIENTS = "INGREDIENTS";
+      public static final String DESCRIPTION = "DESCRIPTION";
+      public static final String NAME = "NAME";
+      private final File indexFile;
+      private final Properties indexManager;
+      public static Map<String,Integer> fieldMap = cakeMap();
 
-						public RandomAccessCakesManager(File file,File indexFile)
-						{
-												super(fieldMap,file);
-												this.indexFile = indexFile;
-												indexManager = new Properties();
-						}
+      public RandomAccessCakesManager(File file,File indexFile)
+      {
+            super(fieldMap,file);
+            this.indexFile = indexFile;
+            indexManager = new Properties();
+      }
 
-						public boolean save(String index,Cake cake) throws IOException
-						{
-												indexManager.load(new FileReader(indexFile));
-												var property = indexManager.getProperty(index,STRING_EMPTY);
-												if (property.isBlank())
-												{
-																		throw new IOException("No existe ese registro");
-												}
-												return super.save(loadCakeData(cake),Long.parseLong(property));
-						}
+      public boolean save(String index,Cake cake) throws IOException
+      {
+            indexManager.load(new FileReader(indexFile));
+            var property = indexManager.getProperty(index,STRING_EMPTY);
+            if (property.isBlank())
+            {
+                  throw new IOException("No existe ese registro");
+            }
+            return super.save(loadCakeData(cake),Long.parseLong(property));
+      }
 
-						@Override
-						public Cake get(String index) throws IOException
-						{
-												indexManager.load(new FileReader(indexFile));
-												var dataIndex = indexManager.getProperty(index,STRING_EMPTY);
-												if (dataIndex.isBlank())
-												{
-																		throw new IOException("No existe ese registro");
-												}
-												return get(Integer.parseInt(dataIndex));
-						}
+      @Override
+      public Cake get(String index) throws IOException
+      {
+            indexManager.load(new FileReader(indexFile));
+            var dataIndex = indexManager.getProperty(index,STRING_EMPTY);
+            if (dataIndex.isBlank())
+            {
+                  throw new IOException("No existe ese registro");
+            }
+            return get(Integer.parseInt(dataIndex));
+      }
 
-						@Override
-						public Cake get(int index) throws IOException
-						{
-												if (file.length() == 0)
-												{
-																		throw new IOException("No hay datos guardados");
-												}
-												try (var accessFile = new RandomAccessFile(file,RWS))
-												{
+      @Override
+      public Cake get(int index) throws IOException
+      {
+            if (file.length() == 0)
+            {
+                  throw new IOException("No hay datos guardados");
+            }
+            try (var accessFile = new RandomAccessFile(file,RWS))
+            {
 
-																		var completeJump = index * recordLength;
-																		if (completeJump > file.length())
-																		{
-																								throw new IOException("OutOfBounds");
-																		}
-																		accessFile.seek(completeJump);
-																		var cake = new Cake();
+                  var completeJump = index * recordLength;
+                  if (completeJump > file.length())
+                  {
+                        throw new IOException("OutOfBounds");
+                  }
+                  accessFile.seek(completeJump);
+                  var cake = new Cake();
 
-																		fieldMap.entrySet()
-																										.forEach(entryData-> buildCake(cake,entryData,accessFile));
-																		return cake;
-												}
+                  fieldMap.entrySet()
+                          .forEach(entryData-> buildCake(cake,entryData,accessFile));
+                  return cake;
+            }
 
-						}
+      }
 
-						private static Map<String,Integer> cakeMap()
-						{
-												var cakeMap = new HashMap<String,Integer>();
-												cakeMap.put(DESCRIPTION,350);
-												cakeMap.put(INGREDIENTS,200);
-												cakeMap.put(PRICE,8);
-												cakeMap.put(NAME,20);
+      private static Map<String,Integer> cakeMap()
+      {
+            var cakeMap = new HashMap<String,Integer>();
+            cakeMap.put(DESCRIPTION,350);
+            cakeMap.put(INGREDIENTS,200);
+            cakeMap.put(PRICE,8);
+            cakeMap.put(NAME,20);
 
-												return cakeMap;
-						}
+            return cakeMap;
+      }
 
-						private void buildCake(Cake cake,Entry<String,Integer> entryData,RandomAccessFile accessFile)
-						{
-												var fieldLength = entryData.getValue();
-												var bytes = new byte[fieldLength];
-												try
-												{
-																		if (accessFile.read(bytes,0,fieldLength) == -1)
-																		{
-																								throw new IOException();
-																		}
-												}
-												catch (IOException ex)
-												{
-																		System.out.println("Error al tratar de obtener el dato");
-												}
-												var cakeField = new String(bytes,Charset.defaultCharset()).strip();
+      private void buildCake(Cake cake,Entry<String,Integer> entryData,RandomAccessFile accessFile)
+      {
+            var fieldLength = entryData.getValue();
+            var bytes = new byte[fieldLength];
+            try
+            {
+                  if (accessFile.read(bytes,0,fieldLength) == -1)
+                  {
+                        throw new IOException();
+                  }
+            }
+            catch (IOException ex)
+            {
+                  System.out.println("Error al tratar de obtener el dato");
+            }
+            var cakeField = new String(bytes,Charset.defaultCharset()).strip();
 
-												switch (entryData.getKey())
-												{
-																		case NAME:
-																								cake.setCakeName(cakeField);
-																								break;
-																		case DESCRIPTION:
-																								cake.setDescription(cakeField);
-																								break;
-																		case INGREDIENTS:
-																								cake.setIngredients(Arrays.asList(cakeField.split(SEPARATOR)));
-																								break;
-																		case PRICE:
-																								cake.setPrice(Float.parseFloat(cakeField));
-																								break;
-												}
-						}
+            switch (entryData.getKey())
+            {
+                  case NAME:
+                        cake.setCakeName(cakeField);
+                        break;
+                  case DESCRIPTION:
+                        cake.setDescription(cakeField);
+                        break;
+                  case INGREDIENTS:
+                        cake.setIngredients(Arrays.asList(cakeField.split(SEPARATOR)));
+                        break;
+                  case PRICE:
+                        cake.setPrice(Float.parseFloat(cakeField));
+                        break;
+            }
+      }
 
-						public boolean save(Cake cake)
-						{
-												var record = loadCakeData(cake);
-												var successfulInsert = super.save(record);
-												if (successfulInsert)
-												{
-																		try
-																		{
-																								indexManager.load(new FileReader(indexFile));
-																								successfulInsert = false;
-																								indexManager.setProperty(record.get(NAME),rowsOfRecords - 1 + STRING_EMPTY);
-																								indexManager.store(new FileWriter(indexFile),"Se ha guardado el registro ID-" + (rowsOfRecords - 1));
-																		}
-																		catch (IOException ex)
-																		{
-																								System.out.println("No se pudieron realizar los cambios");
-																		}
-												}
-												return successfulInsert;
-						}
+      public boolean save(Cake cake)
+      {
+            var record = loadCakeData(cake);
+            var successfulInsert = super.save(record);
+            if (successfulInsert)
+            {
+                  try
+                  {
+                        indexManager.load(new FileReader(indexFile));
+                        successfulInsert = false;
+                        indexManager.setProperty(record.get(NAME),rowsOfRecords - 1 + STRING_EMPTY);
+                        indexManager.store(new FileWriter(indexFile),"Se ha guardado el registro ID-" + (rowsOfRecords - 1));
+                  }
+                  catch (IOException ex)
+                  {
+                        System.out.println("No se pudieron realizar los cambios");
+                  }
+            }
+            return successfulInsert;
+      }
 
-						private HashMap<String,String> loadCakeData(Cake cake)
-						{
-												var ingredients = cake.getIngredients()
-																																		.stream()
-																																		.map(str-> str.concat(SEPARATOR))
-																																		.reduce(STRING_EMPTY,String::concat);
+      private HashMap<String,String> loadCakeData(Cake cake)
+      {
+            var ingredients = cake.getIngredients()
+                                  .stream()
+                                  .map(str-> str.concat(SEPARATOR))
+                                  .reduce(STRING_EMPTY,String::concat);
 
-												var record = new HashMap<String,String>();
-												record.put(NAME,cake.getCakeName());
-												record.put(DESCRIPTION,cake.getDescription());
-												record.put(INGREDIENTS,ingredients);
-												record.put(PRICE,cake.getPrice() + STRING_EMPTY);
-												return record;
-						}
+            var record = new HashMap<String,String>();
+            record.put(NAME,cake.getCakeName());
+            record.put(DESCRIPTION,cake.getDescription());
+            record.put(INGREDIENTS,ingredients);
+            record.put(PRICE,cake.getPrice() + STRING_EMPTY);
+            return record;
+      }
 
 }
